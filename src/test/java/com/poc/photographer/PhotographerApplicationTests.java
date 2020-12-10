4d0b1e04ff082cc64d442899d96e5041bc80992e
Marinell Medina <marinellmedina1@gmail.com>
2020-12-10 10:50:37 +0800

    first commit

---
+++ src/test/java/com/poc/photographer/PhotographerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.photographer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotographerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
